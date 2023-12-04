a7aafb393748511791145fa11e02eb4cd79c1e81
Carlos Varela <caduvarela@gmail.com>
2023-12-04 02:17:18 -0300

    Dockerized Application
    Run "docker-compose up" to launch full application.

--- server/config/sequelize.ts
+++ server/config/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('medcloud_challenge_nine', process.env.MYSQL_USER || 'root', process.env.MYSQL_PASSWORD || 'admin', {
-    host: 'localhost',
+const sequelize = new Sequelize('challenge_nine_database', process.env.MYSQL_USER || 'root', process.env.MYSQL_PASSWORD || 'password', {
+    host: "database",
     port: 3306,
     dialect: 'mysql',
     dialectOptions: {
